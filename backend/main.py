70e834ba7641494b92aae8bb83579c28881b1129
jeouel <jerome.ouellette@gmail.com>
2025-11-01 18:07:03 -0400

    added db routes

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,37 @@ def classify():
 def test():
         return routes.test_db()
 
+@app.route("/createDocument", methods=["POST"])
+def create_doc():
+        data = request.json
+        return routes.create_document(data.get("collection"), data.get("text"), data.get("metadata"))
 
+@app.route("/getDocument", methods=["GET"])
+def get_doc():
+        data = request.json
+        return routes.read_document(data.get("collection"), data.get("id"))
 
-# @app.post("/generatePrompt")
+@app.route("/queryDocument", methods=["GET"])
+def query_doc():
+        data = request.json
+        return routes.query_document(data.get("collection"), data.get("query"), data.get("quantity"), data.get("filter"))
 
-# @app.post("/formatPrompt")
+@app.route("/deleteDocument", methods=["DELETE"])
+def delete_doc():
+        data = request.json
+        return routes.delete_document(data.get("collection"), data.get("id"))
+
+@app.route("/updateMetadata", methods=["PATCH"])
+def update_meta():
+        data = request.json
+        return routes.update_document_metadata(data.get("collection"), data.get("id"), data.get("metadata"))
+
+@app.route("/getCollection", methods=["GET"])
+def get_coll():
+        data = request.json
+        return routes.read_collection(data.get("collection"))
+
+@app.route("/deleteCollection", methods=["DELETE"])
+def delete_coll():
+        data = request.json
+        return routes.delete_collection(data.get("collection"))
