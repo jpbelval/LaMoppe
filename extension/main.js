db18b547c0c1e81ca358aa0d0026d66844149135
yes-laurent <laurentbrochu2003@gmail.com>
2025-11-01 10:42:20 -0400

    merge

--- extension/main.js
+++ extension/main.js
@@ -1,8 +1,20 @@
+document.body.style.border = "5px solid red";
+preSubmit();
+
+
+function preSubmit() {
+    //console.log('script roule');
+    let abc = document.querySelector('form')
+    console.log(abc)
+    document.querySelector('form').addEventListener('onsubmit', async (event) =>  {
+        event.preventDefault();
+        console.log('fonctionne');
+    });
+}
+
 function getText() {
     if (document.getElementById('prompt-textarea'))
         return document.getElementById('prompt-textarea').textContent;
     else
         return null
 }
-
-console.log(getText());
