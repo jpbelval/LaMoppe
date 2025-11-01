27f35ad7c0a038d51550c8a91bf67ba7e9ed6230
yes-laurent <laurentbrochu2003@gmail.com>
2025-11-01 17:53:55 -0400

    integration app.py

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,20 @@ from flask import Flask, request, jsonify
 import os
 import routes
 from flask_cors import CORS
+from intelligence.app import SafetyIntelligence
 
 
 app = Flask(__name__)
+
 CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
 chroma_client = None
 CORS(app, origins="*")
-
+safetyIntelligence = SafetyIntelligence()
 @app.route("/classifyText", methods=["POST"])
 def classify():
         text = request.json.get("prompt")
+        safetyAnalysis = safetyIntelligence.analyze_prompt(text)
+        safetyAnalysis = jsonify(safetyAnalysis)
         return jsonify({
             "message": "Received successfully",
             "received": text
