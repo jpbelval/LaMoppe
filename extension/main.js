1f60bf9c93d8c4de79b20039154d6165fc02a56a
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-11-01 13:22:15 -0400

    prevention V2

--- extension/main.js
+++ extension/main.js
@@ -1,37 +1,66 @@
 document.body.style.border = "5px solid red";
 
+// Variables
 const lockKey = ["Shift", "Alt"]
 let lock = false;
+let prevent = true;
 
+// Event for keydown
 addEventListener("keydown", async (event) => {
     if(lockKey.includes(event.key))
         lock = true;
     if(event.key === "Enter")
         if(!lock && (getText() != '')) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-            event.stopPropagation();
+            checkPrevention(event);
         }
 }, true);
 
+// Event for keyup
 addEventListener("keyup", async (event) => {
     if(lockKey.includes(event.key))
         lock = false;
 })
 
-document.addEventListener("click", (e) => {
-    const btn = e.target.closest(
-      "#composer-submit-button, [data-testid='send-button'], [aria-label='Send prompt']"
-    );
+// Event for the right click on the send button
+document.addEventListener("click", (event) => {
+    const btn = event.target.closest("#composer-submit-button");
     if (!btn) return;
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    e.stopPropagation();
+    checkPrevention(event);
 }, true);
 
+// Get the text in ChatGPT
 function getText() {
     if (document.getElementById('prompt-textarea'))
         return document.getElementById('prompt-textarea').textContent;
     else
         return null
+}
+
+// Check for prevention
+function checkPrevention(event) {
+    if(prevent) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        event.stopPropagation();
+        
+        prevent = checkSafety();
+    }
+    else {
+        prevent = true
+    }
+}
+
+// Check the prompt with AI
+function checkSafety() {
+    // test 
+    let isNotSafe = true;
+    //check
+    //si pas safe
+    // laisse a vrai
+    //sinon
+    // met a faux et reclick
+    if(!isNotSafe) {
+        document.getElementById("composer-submit-button").click();
+    }
+    return isNotSafe;
 }
