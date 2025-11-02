c8552f2563c6807bb27837b8fed4b73f512ca989
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-11-01 21:40:43 -0400

    Notif and extension

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,53 @@
-browser.notifications.create({
-  type: "basic",
-  iconUrl: browser.runtime.getURL("icons/logo48.png"),
-  title: "Nettoyez votre requête",
-  message: "T'es trop dumb pour ecrire à un AI, arrête."
+let count = 0;
+let messages = [];
+
+browser.runtime.onMessage.addListener((message) => {
+    if (message.event === "NewMessage") {
+        sendNotification();
+        messages.push(message.data);
+    }
+    else if (message.event === "subCount") {
+        count = count - 1;
+        updateNotif();
+        messages.splice(messages.indexOf(message.data), 1);
+    }
+    else if (message.event === "fetchData") {
+        browser.runtime.sendMessage({
+            event: "sendData",
+            data: messages
+        });
+    }
 });
+
+function sendNotification() {
+    browser.notifications.create({
+        type: "basic",
+        iconUrl: browser.runtime.getURL("icons/logo48.png"),
+        title: "Nettoyez votre requête",
+        message: "Référez vous à l'extension"
+    });
+
+    count = count + 1;
+    updateNotif();
+}
+
+function sendCustom(text) {
+    browser.notifications.create({
+        type: "basic",
+        iconUrl: browser.runtime.getURL("icons/logo48.png"),
+        title: "Nettoyez votre requête",
+        message: text
+    });
+
+    count = count + 1;
+    updateNotif();
+}
+
+function updateNotif() {
+    if(count) {
+        browser.browserAction.setBadgeText({ text: String(count)});
+        browser.browserAction.setBadgeBackgroundColor({ color: "#FF3B30" });
+    }
+    else
+        browser.browserAction.setBadgeText({text: ""})
+}
